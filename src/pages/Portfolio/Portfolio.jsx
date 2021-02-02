c725278fd80a5a5897c941656c2dec7cf5fc28ab
daniel-takacs <takacsd89@gmail.com>
2021-02-02 07:30:58 +0200

    created index.jsx for exporting images

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -3,31 +3,14 @@ import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import styled from 'styled-components';
 
+import { PTab228b, PTabdelsol, PTabedelweiss, PTabeebox, PTabfederal, PTabhypers, PTabnetcry,
+PTabparamour, PTabprototype, PTabseraph, PTabsxiv, PTabtrinity, 
+PMobile228b, PMobiledelsol, PMobileedelweiss, PMobileeebox, PMobilefederal, PMobilehypers, PMobilenetcry,
+PMobileparamour, PMobileprototype, PMobileseraph, PMobilesxiv, PMobiletrinity,
 
-import PortfolioImg1 from '../../assets/images/portfolio/mobile/image-228b.jpg';
-import PortfolioImg2 from '../../assets/images/portfolio/mobile/image-del-sol.jpg';
-import PortfolioImg3 from '../../assets/images/portfolio/mobile/image-edelweiss.jpg';
-import PortfolioImg4 from '../../assets/images/portfolio/mobile/image-eebox.jpg';
-import PortfolioImg5 from '../../assets/images/portfolio/mobile/image-federal.jpg';
-import PortfolioImg6 from '../../assets/images/portfolio/mobile/image-hypers.jpg';
-import PortfolioImg7 from '../../assets/images/portfolio/mobile/image-netcry.jpg';
-import PortfolioImg8 from '../../assets/images/portfolio/mobile/image-paramour.jpg';
-import PortfolioImg9 from '../../assets/images/portfolio/mobile/image-prototype.jpg';
-import PortfolioImg10 from '../../assets/images/portfolio/mobile/image-seraph.jpg';
-import PortfolioImg11 from '../../assets/images/portfolio/mobile/image-sxiv.jpg';
-import PortfolioImg12 from '../../assets/images/portfolio/mobile/image-trinity.jpg';
+PDesk228b, PDeskdelsol, PDeskedelweiss, PDeskeebox, PDeskfederal, PDeskhypers, PDesknetcry,
+PDeskparamour, PDeskprototype, PDeskseraph, PDesksxiv, PDesktrinity } from '../../assets/images/index';
 
-import PortfolioTabletImg1 from '../../assets/images/portfolio/tablet/image-228b.jpg';
-import PortfolioTabletImg2 from '../../assets/images/portfolio/tablet/image-del-sol.jpg';
-import PortfolioTabletImg3 from '../../assets/images/portfolio/tablet/image-edelweiss.jpg';
-import PortfolioTabletImg4 from '../../assets/images/portfolio/tablet/image-eebox.jpg';
-import PortfolioTabletImg5 from '../../assets/images/portfolio/tablet/image-federal.jpg';
-import PortfolioTabletImg6 from '../../assets/images/portfolio/tablet/image-hypers.jpg';
-import PortfolioTabletImg7 from '../../assets/images/portfolio/tablet/image-netcry.jpg';
-import PortfolioTabletImg8 from '../../assets/images/portfolio/tablet/image-paramour.jpg';
-import PortfolioTabletImg9 from '../../assets/images/portfolio/tablet/image-prototype.jpg';
-import PortfolioTabletImg10 from '../../assets/images/portfolio/tablet/image-seraph.jpg';
-import PortfolioTabletImg11 from '../../assets/images/portfolio/tablet/image-sxiv.jpg';
 //import PortfolioTabletImg12 from '../../assets/images/portfolio/tablet/image-trinity.jpg';
 
 const PortfolioContainer = styled.div `
@@ -92,108 +75,108 @@ function PortfolioPage() {
         <>
             <PortfolioContainer >
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg10}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter" src={PMobileseraph}/>
+                    <PortfolioImage className="imgfilter" src={PTabseraph}/>
+                    <PortfolioImage className="imgfilter" src={PDeskseraph}/>
                     <PortfolioText>
                     <PortfolioH3>Seraph Station</PortfolioH3>
                     <PortfolioP>September 2019</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg4}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg4}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobileeebox}/>
+                    <PortfolioImage className="imgfilter" src={PTabeebox}/>
+                    <PortfolioImage className="imgfilter" src={PDeskeebox}/>
                     <PortfolioText>
                     <PortfolioH3>Eebox Building</PortfolioH3>
                     <PortfolioP>August 2017</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg5}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg5}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobilefederal}/>
+                    <PortfolioImage className="imgfilter" src={PTabfederal}/>
+                    <PortfolioImage className="imgfilter" src={PDeskfederal}/>
                     <PortfolioText>
                     <PortfolioH3>Federal II Tower</PortfolioH3>
                     <PortfolioP>March 2017</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg2}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg2}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobiledelsol}/>
+                    <PortfolioImage className="imgfilter" src={PTabdelsol}/>
+                    <PortfolioImage className="imgfilter" src={PDeskdelsol}/>
                     <PortfolioText>
                     <PortfolioH3>Project Del Sol</PortfolioH3>
                     <PortfolioP>January 2016</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg9}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg9}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobileprototype}/>
+                    <PortfolioImage className="imgfilter" src={PTabprototype}/>
+                    <PortfolioImage className="imgfilter" src={PDeskprototype}/>
                     <PortfolioText>
                     <PortfolioH3>Le Prototype</PortfolioH3>
                     <PortfolioP>October 2015</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg1}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg1}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobile228b}/>
+                    <PortfolioImage className="imgfilter" src={PTab228b}/>
+                    <PortfolioImage className="imgfilter" src={PDesk228b}/>
                     <PortfolioText>
                     <PortfolioH3>228B Tower</PortfolioH3>
                     <PortfolioP>April 2015</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg3}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg3}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobileedelweiss}/>
+                    <PortfolioImage className="imgfilter" src={PTabedelweiss}/>
+                    <PortfolioImage className="imgfilter" src={PDeskedelweiss}/>
                     <PortfolioText>
                     <PortfolioH3>Grand Edelweiss Hotel</PortfolioH3>
                     <PortfolioP>December 2013</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg7}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg6}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobilenetcry}/>
+                    <PortfolioImage className="imgfilter" src={PTabnetcry}/>
+                    <PortfolioImage className="imgfilter" src={PDesknetcry}/>
                     <PortfolioText>
                     <PortfolioH3>Netcry Tower</PortfolioH3>
                     <PortfolioP>August 2012</PortfolioP>
                     </PortfolioText>
                 </Portfolio> 
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg6}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg7}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobilehypers}/>
+                    <PortfolioImage className="imgfilter" src={PTabhypers}/>
+                    <PortfolioImage className="imgfilter" src={PDeskhypers}/>
                     <PortfolioText>
                     <PortfolioH3>Hypers</PortfolioH3>
                     <PortfolioP>January 2012</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg11}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg8}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobilesxiv}/>
+                    <PortfolioImage className="imgfilter" src={PTabsxiv}/>
+                    <PortfolioImage className="imgfilter" src={PDesksxiv}/>
                     <PortfolioText>
                     <PortfolioH3>SXIV Tower</PortfolioH3>
                     <PortfolioP>March 2011</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg12}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg11}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobiletrinity}/>
+                    <PortfolioImage className="imgfilter" src={PTabtrinity}/>
+                    <PortfolioImage className="imgfilter" src={PDesktrinity}/>
                     <PortfolioText>
                     <PortfolioH3>Trinity Bank Tower</PortfolioH3>
                     <PortfolioP>September 2010</PortfolioP>
                     </PortfolioText>
                 </Portfolio>
                 <Portfolio> 
-                    <PortfolioImage className="imgfilter"src={PortfolioImg8}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioTabletImg10}/>
-                    <PortfolioImage className="imgfilter" src={PortfolioImg10}/>
+                    <PortfolioImage className="imgfilter"src={PMobileparamour}/>
+                    <PortfolioImage className="imgfilter" src={PTabparamour}/>
+                    <PortfolioImage className="imgfilter" src={PDeskparamour}/>
                     <PortfolioText>
                     <PortfolioH3>Project Paramour</PortfolioH3>
                     <PortfolioP>February 2008</PortfolioP>

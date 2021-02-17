113709edac7438fcb1eda64740372683ec4234fe
daniel-takacs <takacsd89@gmail.com>
2021-02-17 21:16:06 +0200

    working on form validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -15,19 +15,110 @@ const ContactButtonWrapper = styled.div `
   display: flex;
   justify-content: flex-end;
 `
-function ContactForm() {
+
+const initialState = {
+    name: "",
+    email: "",
+    text: "",
+    nameError: "",
+    emailError: "",
+    textError: "",
+}
+export default class  ContactForm extends React.Component {
+  state = initialState;
+
+  handleChange = (e) => {
+    const isCheckbox = e.target.type === "checkbox";
+    this.setState({
+      [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+    });
+};
+
+validate = () => {
+  let nameError= "";
+  let emailError= "";
+  let textError= "";
+
+  //name error
+
+  if(!this.state.name ) {
+    nameError = "Can't be empty";
+  }
+  if (nameError) {
+    this.setState ({ nameError });
+    return false;
+  }
+
+//email error
+
+  if (!this.state.email.includes('@')){
+    emailError = "Please use a valid email address"
+    ;
+  }
+
+  if (emailError) {
+    this.setState({ emailError });
+    return false;
+  }
+
+//text error
+
+  if (!this.state.text) {
+    textError = "Can't be empty";
+  }
+
+  if(textError) {
+    this.setState({ textError });
+    return false;
+  }
+
+
+  return true;
+}
+
+
+handleSubmit = event => {
+  event.preventDefault();
+  const isValid = this.validate();
+  if(isValid) {
+    console.log(this.state);
+    this.setState(initialState)
+  }
+}
+ 
+  render() {
     return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
-            <input type="text" placeholder="Name"/>
+            <input 
+                name="name" 
+                placeholder="Name" 
+                value={this.state.name} 
+                onChange={this.handleChange}
+                />
+                <div style={{ color: 'red'}}>{this.state.nameError}</div>
           </label>
           <label>  
-            <input type="text" placeholder="Email"/>
+            <input 
+                name="email" 
+                placeholder="Email" 
+                value={this.state.email} 
+                onChange={this.handleChange}/>
+                <div style={{ color: 'red'}}>{this.state.emailError}</div>
           </label>
-          <textarea name="" id="" cols="30" rows="5" placeholder="Message"></textarea>
+          <textarea name="" 
+                    cols="30" 
+                    rows="5" 
+                    placeholder="Message"
+                    value={this.state.text} 
+                    onChange={this.handleChange}>
+          </textarea>
+          <div style={{ color: 'red'}}>{this.state.textError}</div>
           <ContactButtonWrapper><Button><ArrowForwardOutlinedIcon /></Button></ContactButtonWrapper>     
         </form>
     )
+  }
 }
 
-export default ContactForm
